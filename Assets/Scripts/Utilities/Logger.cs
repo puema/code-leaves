43a770a776cc52d862987360f31ad8e6edd1e602
Marcel <marcelpuetz@web.de>
2017-07-28 09:51:59 +0200

    Add logger for formatted json output

---
+++ Assets/Scripts/Utilities/Logger.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+using UnityEngine;
+
+public class Logger
+{
+    public static void Json(object obj)
+    {
+        Debug.Log(JsonConvert.SerializeObject(obj, Formatting.Indented));
+    }
+}
