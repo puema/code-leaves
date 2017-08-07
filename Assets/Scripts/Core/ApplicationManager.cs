16467b952e3051c4f1c36a1bc601b7803e19ebe8
Marcel <marcelpuetz@web.de>
2017-08-07 22:39:03 +0200

    Work on business models

---
+++ Assets/Scripts/Core/ApplicationManager.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using HoloToolkit.Unity;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class ApplicationManager : Singleton<ApplicationManager>
+{
+    public UiNode Root;
+    
+    private const string TreeDataFilePath = "Assets/StreamingAssets/TreeStructureTypes.json";
+    private readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
+    {
+        TypeNameHandling = TypeNameHandling.Auto
+    };
+
+    private void Start()
+    {
+        Root = DesirializeData();
+        InteractionManager.Instance.Root = Root;
+
+//        SerializeData(Root);
+
+        TreeBuilder.Instance.GenerateTreeStructure((UiInnerNode) Root);
+    }
+    
+    private void SerializeData(object obj)
+    {
+        var json = JsonConvert.SerializeObject(obj, Formatting.Indented, JsonSerializerSettings);
+        File.WriteAllText(TreeDataFilePath, json);
+    }
+
+    private UiInnerNode DesirializeData()
+    {
+        if (!File.Exists(TreeDataFilePath))
+        {
+            Debug.LogError("Connot load tree data, for there is no such file.");
+            return null;
+        }
+        var json = File.ReadAllText(TreeDataFilePath);
+        return JsonConvert.DeserializeObject<UiInnerNode>(json, JsonSerializerSettings);
+    }
+}
---
+++ Assets/Scripts/Core/ApplicationModels.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Snapshot
+{
+    public string Name { get; set; }
+    public DateTime Timestamp { get; set; }
+    public TimeSpan TimeSpan { get; set; }
+    public Dictionary<string, double> SnapshotProperties { get; set; }
+    public Node Root { get; set; }
+}
+
+public abstract class Node
+{
+    public string Name { get; set; }
+    public string Key { get; set; }
+    public Edge Edge { get; set; }
+}
+
+public class InnerNode : Node
+{
+    public List<Node> Children { get; set; }
+}
+
+public class Leaf : Node
+{
+    public List<LeafData> LeafData { get; set; }
+}
+
+public class Edge
+{
+    public Dictionary<string, AggregatedConnection> Connections { get; set; }
+}
+
+public class AggregatedConnection
+{
+    public List<string> IngoingConnections { get; set; }
+    public List<string> OutgoingConnections { get; set; }
+}
+
+public abstract class DirectConnections
+{
+    public string Key { get; set; }
+    public string Info { get; set; }
+    public string Start { get; set; }
+    public string End { get; set; }
+}
+
+public struct LeafData
+{
+    public string Key { get; set; }
+    public double Value { set; get; }
+    public List<string> Infos { get; set; }
+}
+
+public struct InnerNodeData
+{
+    public string Key { get; set; }
+    public double Value { set; get; }
+    public List<string> Infos { get; set; }
+}
