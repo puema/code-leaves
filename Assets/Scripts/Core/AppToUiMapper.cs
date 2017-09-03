8426d6c30ffc683839429b7129c866ea8eb0402d
Marcel <marcelpuetz@web.de>
2017-09-03 22:29:00 +0200

    Add model mappers

---
+++ Assets/Scripts/Core/AppToUiMapper.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Frontend;
+using UniRx;
+using UnityEngine;
+
+namespace Core
+{
+    public static class AppToUiMapper
+    {
+        public static UiNode Map(Node node)
+        {
+            if (node is InnerNode)
+            {
+                return new UiInnerNode
+                {
+                    Id = node.Key,
+                    IsFocused = new ReactiveProperty<bool>(false),
+                    IsSelected = new ReactiveProperty<bool>(false),
+                    Text = new ReactiveProperty<string>(node.Name),
+                    Children = ((InnerNode)node).Children.Select(Map).ToList()
+                };
+            }
+            
+            if (node is Leaf)
+            {
+                return new UiLeaf
+                {
+                    Id = node.Key,
+                    IsFocused = new ReactiveProperty<bool>(false),
+                    IsSelected = new ReactiveProperty<bool>(false),
+                    Text = new ReactiveProperty<string>(node.Name),
+                    Color = new ReactiveProperty<Color?>(HexToNullableColor("#208000"))
+                };
+            }
+            
+            return null;
+        }
+        
+        public static Color? HexToNullableColor(string hexcolor)
+        {
+            Color color;
+            return ColorUtility.TryParseHtmlString(hexcolor, out color) ? new Color?(color) : null;
+        }
+    }
+}
---
+++ Assets/Scripts/Data/SoftwareArtefactToNodeMapper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core;
+
+namespace Data
+{
+    public static class SoftwareArtefactToNodeMapper
+    {
+        public static Node Map(SoftwareArtefact artefact)
+        {
+            if (artefact == null) return null;
+            
+            if (artefact.Children.Count > 0)
+            {
+                return new InnerNode
+                {
+                    Name = artefact.Name,
+                    Key = artefact.Key,
+                    Edge = null,
+                    Children = artefact.Children.Select(Map).ToList(),
+                    Data = new List<InnerNodeData>()
+                };
+            }
+
+
+            if (artefact.Children == null || artefact.Children.Count == 0)
+            {
+                return new Leaf
+                {
+                    Name = artefact.Name,
+                    Key = artefact.Key,
+                    Edge = null,
+                    Data = new List<LeafData>()
+                };
+            }
+
+            return null;
+        }
+    }
+}
